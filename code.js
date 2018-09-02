f59f44bc2bbceb0ff700352cdd86fbe6137977ea
Dmytro Mamenko <mamenko.dv@gmail.com>
2018-09-02 14:59:52 +0200

    zadacha reshena

---
+++ code.js
@@ -0,0 +1,11 @@
+function init () {
+setTimeout(ag,3000);
+ function ag () {
+	let access = document.getElementById("code5");
+ let code ="НАстюшка кицюшка";
+access.innerHTML+=code;
+}
+
+}
+window.onload = init ;
+// alert (code);
